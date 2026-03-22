6a8cab2fd4fe0551f080caf851792f2e76cb1ff5
Hannes <129322998+JvR-Hannes@users.noreply.github.com>
2026-03-22 12:57:24 +0200

    Added Google Measurement ID script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { getActivePromotion } from "@/lib/promotions";
 import Link from "next/link";
 import CartIndicator from "@/cart-indicator";
 import PromotionCountdown from "@/app/components/promotion-countdown";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -138,6 +139,19 @@ export default async function RootLayout({
             </div>
           </footer>
         </CartProvider>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+  `}
+        </Script>
       </body>
     </html>
   );
